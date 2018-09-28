f2d7c761a3e7d7cbb47b5058ef9edcc30503e5ed
farverio <github email address>
2018-09-28 15:04:49 -0500

    added dog controller and initial route

--- backend/app/controllers/user_dog_controller.rb
+++
@@ -1,2 +0,0 @@
-class UserDogController < ApplicationController
-end
--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -11,9 +11,7 @@ def seed_dogs
 end
 
 ryhan = User.create(username: "RyhanC", password: "ryhan", email: "ryhan@example.com", zip_code: 77407)
-
 angela = User.create(username: "AngelaD", password: "angela", email: "angela@example.com", zip_code: 77002)
-
 stephen = User.create(username: "StephenF", password: "stephen", email: "stephen@example.com", zip_code: 77002)
 
 seed_dogs
