1b58efdc23f2b64cf5efd0343e810097d40f2f0a
farverio <github email address>
2018-09-28 09:29:34 -0500

    updated postgres files

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,7 +1,7 @@
 Dog.delete_all
 
 def seed_dogs
-  all_dogs = PetFinderApi.get_dogs_array
+  all_dogs = PetFinderApi.get_dogs_array(77002)
 
   all_dogs.each do |dog_hash|
     Dog.first_or_create(dog_hash)
